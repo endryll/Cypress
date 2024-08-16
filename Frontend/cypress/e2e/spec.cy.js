10e5f47e7760f147cff3428b24a51ed75e5476af
endryll <endryll.silverio.dantas@gmail.com>
2024-08-16 14:25:02 -0300

    adição de testes para todas as imagens e textos.

--- Frontend/cypress/e2e/spec.cy.js
+++ Frontend/cypress/e2e/spec.cy.js
@@ -4,5 +4,21 @@ describe('Página Principal', () => {
   })
   it('Deve renderizar h1 com o texto correto!', () => {
     cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    cy.getByData('texto01').contains('Vantagens do nosso banco:')
+  })
+  it('Deve renderizar h3 com o texto correto!', () => {
+    cy.getByData('texto02').contains('Conta e cartão gratuitos')
+    cy.getByData('texto03').contains('Saques sem custo')
+    cy.getByData('texto04').contains('Programa de pontos')
+    cy.getByData('texto05').contains('Seguro Dispositivos')
+  })
+  it('Deve renderizar p com o texto correto!', () => {
+    cy.getByData('paragrafo01').contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
+    cy.getByData('paragrafo02').contains('Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.')
+    cy.getByData('paragrafo03').contains('Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!')
+    cy.getByData('paragrafo04').contains('Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica.')
+  })
+  it('Deve renderizar img com uma imagen', () => {
+    cy.getByData('imagem').should('be.visible')
   })
 })
--- Frontend/src/paginas/Inicio/index.jsx
+++ Frontend/src/paginas/Inicio/index.jsx
@@ -16,45 +16,45 @@ export default function Inicio() {
               Experimente mais liberdade no controle da sua vida financeira.
               Crie sua conta com a gente!
             </h1>
-            <img
+            <img data-test="imagem"
               src={ilustracaoBanner}
               alt="Gráficos e uma pessoa com dinheiro na mão"
             />
           </div>
           <div className={estilos.vantagens}>
-            <h1>Vantagens do nosso banco:</h1>
+            <h1 data-test="texto01">Vantagens do nosso banco:</h1>
             <div className={estilos.vantagens__itens}>
               <div className={estilos.vantagens__item}>
-                <img src={presente} alt="ícone de um presente" />
-                <h3>Conta e cartão gratuitos</h3>
-                <p>
+                <img data-test="imagem" src={presente} alt="ícone de um presente" />
+                <h3 data-test="texto02">Conta e cartão gratuitos</h3>
+                <p data-test="paragrafo01">
                   Isso mesmo, nossa conta é digital, sem custo fixo e mais que
                   isso: sem tarifa de manutenção.
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
-                <img src={saque} alt="ícone de uma mão segurando um dinheiro" />
-                <h3>Saques sem custo</h3>
-                <p>
+                <img data-test="imagem" src={saque} alt="ícone de uma mão segurando um dinheiro" />
+                <h3 data-test="texto03">Saques sem custo</h3>
+                <p data-test="paragrafo02">
                   Você pode sacar gratuitamente 4x por mês de qualquer Banco
                   24h.
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
-                <img src={estrela} alt="ícone de uma estrela" />
-                <h3>Programa de pontos</h3>
-                <p>
+                <img data-test="imagem" src={estrela} alt="ícone de uma estrela" />
+                <h3 data-test="texto04">Programa de pontos</h3>
+                <p data-test="paragrafo03">
                   Você pode acumular pontos com suas compras no crédito sem
                   pagar mensalidade!
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
-                <img
+                <img data-test="imagem"
                   src={dispositivo}
                   alt="ícone de um dispositivo como notebook"
                 />
-                <h3>Seguro Dispositivos</h3>
-                <p>
+                <h3 data-test="texto05">Seguro Dispositivos</h3>
+                <p data-test="paragrafo04">
                   Seus dispositivos móveis (computador e laptop) protegidos por
                   uma mensalidade simbólica.
                 </p>
