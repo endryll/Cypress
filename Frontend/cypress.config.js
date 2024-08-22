909d6017a7bfa8f3429292edd998b88feb2920c3
endryll <endryll.silverio.dantas@gmail.com>
2024-08-22 15:04:21 -0300

    verificacao de cadastro

--- Frontend/cypress.config.js
+++ Frontend/cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
+
+module.exports = defineConfig({
+  e2e: {
     baseUrl: 'http://localhost:3000' 
   }, 
 });
