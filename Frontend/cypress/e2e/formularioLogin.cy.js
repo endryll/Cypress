09049a60a8430a3b49507843a9d040a1b7593d5b
endryll <endryll.silverio.dantas@gmail.com>
2024-08-22 10:57:46 -0300

    Implantação de teste de autenticação

---
+++ Frontend/cypress/e2e/formularioLogin.cy.js
@@ -0,0 +1,13 @@
+describe('Formulário de login', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    
+  })
+  it('Não deve permitir um e-mail inválido!', () => {
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
+  })
+})
