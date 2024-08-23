858638c8c8777195d31dd86cccde7afcc3674ea6
endryll <endryll.silverio.dantas@gmail.com>
2024-08-23 11:37:09 -0300

    validando se está redirecionando para a página correta

--- Frontend/cypress/e2e/paginas.cy.js
+++ Frontend/cypress/e2e/paginas.cy.js
@@ -6,16 +6,22 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('senha-input').type('123456')
     cy.getByData('botao-enviar').click()
     cy.location('pathname').should('eq','/home')
-
+  })
+  
+  it('Deve verificar a página de cartôes', () => {
     //catoes
     cy.getByData('app-home').find('a').eq(1).click()
     cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
     cy.location('pathname').should('eq','/home/cartoes')
-
+  })
+    
+  it('Deve verificar a página de cartôes', () => {
     //serviços
     cy.getByData('app-home').find('a').eq(2).click()
     cy.location('pathname').should('eq','/home/servicos')
+  })
 
+  it('Deve verificar a página de cartôes', () => {
     //investimentos
     cy.getByData('app-home').find('a').eq(3).click()
     cy.getByData('titulo-investimentos').should('exist').and('have.text', 'Investimentos')
